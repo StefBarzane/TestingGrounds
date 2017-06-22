d673e1611e5d78132faec1726e53a930cc06ca78
Steve Barnes <hanikamiya_uk@hotmail.com>
2017-06-22 16:41:56 +0100

    Initial Commit no Content

---
+++ Source/MyFPS/MyFPS.h
@@ -0,0 +1,9 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#ifndef __MYFPS_H__
+#define __MYFPS_H__
+
+#include "EngineMinimal.h"
+
+
+#endif
---
+++ Source/MyFPS/MyFPSHUD.h
@@ -0,0 +1,22 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+#pragma once 
+#include "GameFramework/HUD.h"
+#include "MyFPSHUD.generated.h"
+
+UCLASS()
+class AMyFPSHUD : public AHUD
+{
+	GENERATED_BODY()
+
+public:
+	AMyFPSHUD();
+
+	/** Primary draw call for the HUD */
+	virtual void DrawHUD() override;
+
+private:
+	/** Crosshair asset pointer */
+	class UTexture2D* CrosshairTex;
+
+};
+
